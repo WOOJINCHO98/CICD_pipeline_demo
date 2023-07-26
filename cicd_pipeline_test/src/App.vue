77c121028a510ef5014b07fc4689d49a02404671
조우진 <steamedbunwith@gmail.com>
2023-07-26 15:25:30 +0900

    develop branch test

--- cicd_pipeline_test/src/App.vue
+++ cicd_pipeline_test/src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="!!DEVELOP branch + dev" />
+  <HelloWorld msg="!DEVELOP branch + dev" />
 </template>
 
 <style scoped>
